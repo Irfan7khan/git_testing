885d2f31a5872727a18571edbfc98d9d76571ecf
Irfan Khan <irfan7khan123@gmail.com>
2025-08-29 11:17:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium voluptatibus cumque inventore optio maiores tempore, eius quisquam ea molestias aliquam, repellat consectetur praesentium deleniti nobis quas saepe doloremque qui voluptatum.
     </div>
   );
 }
